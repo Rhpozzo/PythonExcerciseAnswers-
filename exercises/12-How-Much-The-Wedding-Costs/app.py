267042982f8721e36fd429a137b25917101227b2
Román Pozzo <rhpozzo@gmail.com>
2020-01-07 23:36:40 +0000

    16 & 17 answers

--- exercises/12-How-Much-The-Wedding-Costs/app.py
+++ exercises/12-How-Much-The-Wedding-Costs/app.py
@@ -1,5 +1,13 @@
 user_input = int(input('How many people are coming to your wedding?\n'))
 
+if user_input <= 50:
+    price = '$4,000'
+elif user_input <= 100:
+    price = '$10,000'
+elif user_input <= 200:
+    price = '$15,000'
+else:
+    price = '$20,000'
 
 
 
---
+++ exercises/16-Create-A-For-Loop/app.py
@@ -0,0 +1,5 @@
+def standards_maker():
+    for x in range(300):
+        print("I will write questions if I am stuck")
+        
+standards_maker()
